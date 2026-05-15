fabbf80b556099c64df817a0cef5b5a264b8b57b
brandon-kigen <kigenbrandon@gmail.com>
2026-05-15 13:39:03 +0300

    feat: add new logo and robots.txt assets and remove unused colocated hooks from app.js

--- blog/assets/js/app.js
+++ blog/assets/js/app.js
@@ -22,7 +22,6 @@ import "phoenix_html";
 // Establish Phoenix Socket and LiveView configuration.
 import { Socket } from "phoenix";
 import { LiveSocket } from "phoenix_live_view";
-import { hooks as colocatedHooks } from "phoenix-colocated/blog";
 import topbar from "../vendor/topbar";
 
 
@@ -112,7 +111,6 @@ const liveSocket = new LiveSocket("/live", Socket, {
   longPollFallbackMs: 2500,
   params: { _csrf_token: csrfToken },
   hooks: {
-    ...colocatedHooks,
     CommentsManager,
     ViewTracker,
     TrixEditor,
